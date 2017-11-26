99853b63d17223e3339db947d9ba66558a8c5baf
Dawnight <weizhiqimail@foxmail.com>
2017-11-26 12:51:24 -0800

    all buffer files

--- 04node_files/08buffer.js
+++ 04node_files/08buffer.js
@@ -8,4 +8,15 @@ console.log('str.length', str.length)//4
 console.log('buf.length', buf.length)//12
 var subBuf = buf.slice(2, 4);
 console.log('buf', buf)
-console.log('subBuf', subBuf)
+console.log('subBuf', subBuf);
+console.log('=======buf.write()=====')
+var road = new Buffer(100).fill(0);
+road.write('路',0,3);
+console.log(road);
+console.log(road.toString())
+
+//可以使用StringDecoder对象将Buffer对象中的数据转换为字符串
+
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf-8')
+console.log(decoder.write(road))
---
+++ 04node_files/10BufferClass.js
@@ -0,0 +1,25 @@
+// isBuffer()
+var buf = new Buffer(10).fill(0);
+console.log(Buffer.isBuffer(buf));//true
+
+//byteLength
+var strChinese = new Buffer('对象')
+console.log(buf.byteLength);//10
+console.log(strChinese.byteLength);//6
+console.log(buf.toString().length);//10
+console.log(strChinese.toString().length);//2
+
+//concat
+var strChineseA = new Buffer('汉字')
+var strChineseB = new Buffer('华夏')
+var strChineseC = new Buffer('美食佳肴')
+var strChineseAll = Buffer.concat([strChineseA, strChineseB, strChineseC])
+console.log(strChineseAll)
+//<Buffer e6 b1 89 e5 ad 97 e5 8d 8e e5 a4 8f e7 be 8e e9 a3 9f e4 bd b3 e8 82 b4>
+console.log(strChineseAll.toString());//汉字华夏美食佳肴
+
+//isEncoding 检测一个字符串是否为一个有效的编码格式字符串
+var one = 'utf-8'
+var two = 'utf-16'
+console.log(Buffer.isEncoding(one));//true
+console.log(Buffer.isEncoding(two));//false
