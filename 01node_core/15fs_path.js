7ac8b84378cc0a4f0b71b42ad3401577faa0574a
Carl <weizhiqimail@foxmail.com>
2018-01-13 16:50:16 +0800

    利用fs和path，遍历文件夹里所有的文件和目录，并判断某一个文件里是否有某一些内容

--- 01node_core/15fs_path.js
+++ 01node_core/15fs_path.js
@@ -45,3 +45,45 @@ console.log(path.sep);
 console.log(path.delimiter);
 //    :   linux
 //    ;   windows
+
+// 遍历文件夹里所有的文件和目录，并判断某一个文件里是否有某一些内容
+let filePath = path.resolve('/root/files/order');
+fileDisplay(filePath);
+function fileDisplay(filePath){
+    fs.readdir(filePath, (err, files) => {
+        // 这里的files是根目录下的直接子文件和子目录
+        if(err){
+            console.warn(err);
+        }else{
+            files.forEach((filename) => {
+                // 对根目录下的直接子文件和子目录进行遍历
+                // 利用path的拼接路径方法
+                // 找到该文件的路径
+                let fileDir = path.join(filePath, filename);
+                console.log(fileDir);
+                fs.stat(fileDir, (err, stats) => {
+                    // 利用fs的stat方法判断遍历的对象是文件还是目录
+                    if(err){
+                        console.warn('get file stats info failed');
+                    }else{
+                        let isFile = stats.isFile();
+                        let isDir = stats.isDirectory();
+                        if(isFile){
+                            if(fileDir.indexOf('user.js') > 0 ){
+                                fs.readFile(fileDir, (err, targetFileData)=>{
+                                    if(targetFileData.indexOf("var id = '") > 0){
+                                        console.log('the user.js has id');
+                                    }else{
+                                        console.log('the user.js has not id', fileDir);
+                                    }
+                                })
+                            }
+                        }else{
+                            fileDisplay(fileDir);
+                        }
+                    }
+                })
+            })
+        }
+    })
+}
