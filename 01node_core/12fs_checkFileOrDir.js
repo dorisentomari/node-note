d2f9c030090bbf957d969386729465b6686b4abc
Dawnight <weizhiqimail@foxmail.com>
2018-02-23 22:42:30 +0800

    文件系统目录操作与文件信息

--- 01node_core/12fs_checkFileOrDir.js
+++ 01node_core/12fs_checkFileOrDir.js
@@ -2,48 +2,48 @@ var fs = require('fs');
 
 var mkdir = './mkdirOne'
 //创建目录
-fs.mkdir(mkdir, function(err){
-	if(err){//如果有该目录,就创建失败
+fs.mkdir(mkdir, function (err) {
+	if (err) {//如果有该目录,就创建失败
 		console.log(`mkdir ${mkdir} file failed`);
-	}else{
+	} else {
 		console.log(`mkdir ${mkdir} file success`)
 	}
 })
 
-fs.mkdir(mkdir + '/one', function(err){
-	if(err){//如果有该目录,就创建失败
+fs.mkdir(mkdir + '/one', function (err) {
+	if (err) {//如果有该目录,就创建失败
 		console.log(`mkdir ${mkdir} /one file failed`);
-	}else{
+	} else {
 		console.log(`mkdir ${mkdir} /one file success`)
 	}
 })
 
-fs.mkdir(mkdir + '/two', function(err){
-	if(err){//如果有该目录,就创建失败
+fs.mkdir(mkdir + '/two', function (err) {
+	if (err) {//如果有该目录,就创建失败
 		console.log(`mkdir ${mkdir} /two file failed`);
-	}else{
+	} else {
 		console.log(`mkdir ${mkdir} /two file success`)
 	}
 })
 
 var writeData = 'The engaged couple will appear\r\n for a photo outside Kensington Palace \r\non Monday afternoon, and will take part in a broadcast interview in the evening.'
-fs.writeFile(mkdir + '/one'+ '/one.txt', writeData, 'utf-8', function(err){
-	if(err){
+fs.writeFile(mkdir + '/one' + '/one.txt', writeData, 'utf-8', function (err) {
+	if (err) {
 		console.log(`writeFile ${mkdir}/one/one.txt file failed`);
-	}else{
+	} else {
 		console.log(`writeFile ${mkdir}/one/one.txt file success`);
 	}
 })
 //fs.mkdirSync(path, [mode])同步创建文件夹,方法一样
 
-fs.readdir(mkdir, function(err, files){
-	if(err){
+fs.readdir(mkdir, function (err, files) {
+	if (err) {
 		console.log(`readdir ${mkdir} file failed`);
-	}else{
+	} else {
 		console.log(`readdir ${mkdir} file success`);
 		console.log(files);
 	}
-})
+});
 
 //fs.readdirSync()
 
@@ -52,10 +52,10 @@ fs.readdir(mkdir, function(err, files){
 //fs.stat(path, callback(err, stats))
 //fs.lstat(path, callback(err, stats))
 //查看文件信息
-fs.stat('./one', function(err, stats){
-	if(err){
+fs.stat('./one', function (err, stats) {
+	if (err) {
 		console.log('fs.stat("./one") file failed');
-	}else{
+	} else {
 		console.log(stats);
 		console.log('fs.stat("./one") file success');
 	}
@@ -92,23 +92,23 @@ Stats {
 // var stats = fs.fstatSync(fd) 
 
 //检查文件或目录是否存在
-fs.exists(mkdir, function(exists){
-	if(exists){
+fs.exists(mkdir, function (exists) {
+	if (exists) {
 		console.log(exists)
 		console.log(`${mkdir} file exists`)
-	}else{
+	} else {
 		console.log(`${mkdir} file is not exists`)
 	}
-})
+});
 
 // 获取文件或目录的绝对路径
 // resolvedPath  代表的是获取的文件或目录的绝对路径
 // fs.realpath(path, [cache], callback(err, resolvedPath))
 
-fs.realpath(mkdir, function(err, resolvedPath){
-	if(err){
+fs.realpath(mkdir, function (err, resolvedPath) {
+	if (err) {
 		console.log(`${mkdir} file or dir is not exists`)
-	}else{
+	} else {
 		console.log(`${mkdir} realpath is ${resolvedPath}`)
 	}
 })
@@ -116,15 +116,15 @@ fs.realpath(mkdir, function(err, resolvedPath){
 //修改文件访问时间及修改时间
 // fs.utimes(path, atime, mtime, callback(err))
 // 修改文件访问时间及修改时间都为当前时间
-fs.utimes(mkdir, new Date(), new Date(), function(err){
-	if(err){
+fs.utimes(mkdir, new Date(), new Date(), function (err) {
+	if (err) {
 		console.log(`utimes ${mkdir} file failed`);
-	}else{
+	} else {
 		console.log(`utimes ${mkdir} file success`);
 	}
 })
 
-fs.stat(mkdir, function(err, stats){
+fs.stat(mkdir, function (err, stats) {
 	console.log(stats)
 })
 
@@ -132,17 +132,17 @@ fs.stat(mkdir, function(err, stats){
 
 //使用open方法或者openSync方法打开文件并返回文件描述符后，可以使用fs模块中的futimes方法修改文件的访问时间或者修改时间
 
-fs.open('./file02.txt', 'r', function(err, fd){
-	fs.futimes(fd, new Date(), new Date(), function(err){
-		if(err){
-		console.log(`futimes file02.txt file failed`);
-		}else{
+fs.open('./file02.txt', 'r', function (err, fd) {
+	fs.futimes(fd, new Date(), new Date(), function (err) {
+		if (err) {
+			console.log(`futimes file02.txt file failed`);
+		} else {
 			console.log(`futimes file02.txt file success`);
 		}
 	})
 })
 
-fs.stat('./file02.txt', function(err, stats){
+fs.stat('./file02.txt', function (err, stats) {
 	console.log(stats)
 })
 
@@ -153,10 +153,10 @@ fs.stat('./file02.txt', function(err, stats){
 // mode代表的是权限的大小
 // fs.chmod方法触发前的权限是`drwxr-xr-x.`
 // fs.chmod方法触发后的权限是`drw-------.`
-fs.chmod(mkdir, 0600, function(err){
-	if(err){
+fs.chmod(mkdir, '0600', function (err) {
+	if (err) {
 		console.log(`fs.chmod ${mkdir} file failed`);
-	}else{
+	} else {
 		console.log(`fs.chmod ${mkdir} file success`);
 	}
 })
@@ -167,14 +167,14 @@ fs.chmod(mkdir, 0600, function(err){
 
 // fs.fchmod方法触发前的权限是`-rw-r--r--.`
 // fs.fchmod方法触发后的权限是`-rw-------.`
-fs.open('./file02.txt', 'r', function(err, fd){
-	if(err){
+fs.open('./file02.txt', 'r', function (err, fd) {
+	if (err) {
 		console.log(`./file02.txt file open failed`);
-	}else{
-		fs.fchmod(fd, 0600, function(err){
-			if(err){
+	} else {
+		fs.fchmod(fd, 0600, function (err) {
+			if (err) {
 				console.log(`fs.fchmod ./file02.txt file failed`);
-			}else{
+			} else {
 				console.log(`fs.fchmod ./file02.txt file success`);
 			}
 		})
