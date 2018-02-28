ab544ad4b440d5bc73b1cebf5e1f638796552564
Dawnight <weizhiqimail@foxmail.com>
2018-02-28 23:09:01 +0800

    删除多余的信息

--- 04mongoose/connect.js
+++ 04mongoose/connect.js
@@ -1,16 +1,16 @@
 //引入mongoose模块
-var mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const DB_URL="mongodb://127.0.0.1:27017/infos";
-mongoose.Promise=global.Promise;
+const DB_URL = "mongodb://127.0.0.1:27017/infos";
+mongoose.Promise = global.Promise;
 
-mongoose.connect(DB_URL,{useMongoClient:true});
+mongoose.connect(DB_URL, {useMongoClient: true});
 
-mongoose.connection.on("connected",()=>{
-    console.log("mongodb数据库连接成功")
+mongoose.connection.on("connected", () => {
+	console.log("mongodb数据库连接成功");
 });
 
-mongoose.connection.on("error",(error)=>{
-    console.log("mongodb数据库连接失败",error)
+mongoose.connection.on("error", (error) => {
+	console.log("mongodb数据库连接失败", error);
 });
-module.exports=mongoose;
+module.exports = mongoose;
